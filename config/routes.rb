ca64c084f172811046c32066634e6474a6423647
Rennerp <rekefa@gmail.com>
2021-03-11 20:07:37 -0600

    Adds update action the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # We are going to list our resources here
-      resources :users, only: %i[create show]
+      resources :users, only: %i[create show update]
     end
   end
 end
